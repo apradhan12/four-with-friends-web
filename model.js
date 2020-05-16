ae67d305e0f0959388b5b8a24b6bf3270fed7e83
Aaron Pradhan <apradhan@tripadvisor.com>
2020-05-15 21:35:17 -0400

    Removed STATES object

--- model.js
+++ model.js
@@ -15,121 +15,6 @@ const DOM_ID_MAP = {
     exitToLobby: "#exitLobbyDiv"
 };
 
-const STATES = {
-    inLobby: {
-        visible: [
-            DOM_ID_MAP.status,
-            DOM_ID_MAP.lobbyButtons
-        ],
-        states: {
-            selecting: {
-                visible: [],
-                status: "Click one of the buttons to start playing."
-            },
-            creatingPrivate: {
-                visible: [
-                    DOM_ID_MAP.username
-                ],
-                status: "Create a username."
-            },
-            joiningPrivate: {
-                visible: [
-                    DOM_ID_MAP.username,
-                    DOM_ID_MAP.gameCode
-                ],
-                status: "Create a username and input the code of the game you wish to join."
-            },
-            joiningOpen: {
-                visible: [
-                    DOM_ID_MAP.username
-                ],
-                status: "Create a username."
-            }
-        }
-    },
-    waiting: {
-        visible: [
-            DOM_ID_MAP.status,
-            DOM_ID_MAP.exitToLobby
-        ],
-        states: {
-            waitingOpponentOpen: {
-                visible: [],
-                status: "Waiting for an opponent..."
-            },
-            waitingOpponentPrivate: {
-                visible: [
-                    DOM_ID_MAP.gameCodeDisplay
-                ],
-                status: "Waiting for an opponent..."
-            }
-        }
-    },
-    inGame: {
-        visible: [
-            DOM_ID_MAP.canvas,
-            DOM_ID_MAP.status,
-            DOM_ID_MAP.gameInfo,
-            DOM_ID_MAP.moveColumn,
-            DOM_ID_MAP.exitToLobby
-        ],
-        states: {
-            yourMove: {
-                visible: [
-                    DOM_ID_MAP.moveColumn
-                ],
-                status: "It's your turn."
-            },
-            opponentMove: {
-                visible: [],
-                status: "It's the other player's turn."
-            }
-        }
-    },
-    postGame: {
-        visible: [
-            DOM_ID_MAP.canvas,
-            DOM_ID_MAP.status,
-            DOM_ID_MAP.gameInfo,
-            DOM_ID_MAP.exitToLobby
-        ],
-        states: {
-            opponentDisconnected: {
-                visible: [],
-                status: "The opponent has disconnected."
-            },
-            win: {
-                visible: [
-                    DOM_ID_MAP.rematch
-                ],
-                status: "You won!"
-            },
-            loss: {
-                visible: [
-                    DOM_ID_MAP.rematch
-                ],
-                status: "You lost."
-            },
-            draw: {
-                visible: [
-                    DOM_ID_MAP.rematch
-                ],
-                status: "It's a draw."
-            },
-            opponentRequestedRematch: {
-                visible: [
-                    DOM_ID_MAP.rematch
-                ],
-                status: "The opponent has requested a rematch."
-            },
-            playerRequestedRematch: {
-                visible: [],
-                status: "Waiting for the opponent's response..."
-            }
-        }
-    }
-};
-
 const GAME_STATES = {
     playerTurn: "playerTurn",
     opponentTurn: "opponentTurn"
@@ -146,7 +31,7 @@ const GAME_RESULTS = {
 
 class StateModel {
     constructor() {
-        this.state = STATES.inLobby;
+        this.state = new Selecting();
         this.username = null;
         this.opponentUsername = null;
         this.gameModel = null;
